04ac65cf54eda3f64120ef459985a29feb429854
Butch Howard <butchhoward@gmail.com>
2021-05-23 14:20:26 -0400

    reduce the use of Find by using memory
    seems to improve the performance a good bit

--- training/role.harvester.js
+++ training/role.harvester.js
@@ -1,37 +1,68 @@
 
 var roleUpgrader = require('role.upgrader');
 
-module.exports = roleHarvester;
+
+function directCreepToWork(creep) {
+  var creepData = Memory.creeps[creep.name];
+
+  if (creep.store.getFreeCapacity() > 0) {
+    if (creepData.sourceId != undefined) {
+      const source = Game.getObjectById(creepData.sourceId);
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
+    }
+  }
+  else {
+    if (creepData.targetId != undefined) {
+      const source = Game.getObjectById(creepData.targetId);
+      if (creep.transfer(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, { visualizePathStyle: { stroke: '#ffffff' } });
+      }
+    }
+    else {
+      // help with the upgrading when they have nothing else to do
+      roleUpgrader.run(creep);
+    }
+  }
+}
+
+function setCreepTargets(creep) {
+  var creepData = Memory.creeps[creep.name];
+
+  if (creepData.sourceId == undefined) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if (sources.length > 0) {
+      creepData.sourceId = sources[0].id;
+    }
+  }
+
+  if (creepData.targetId == undefined) {
+    var targets = creep.room.find(FIND_STRUCTURES, { filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || 
+                                structure.structureType == STRUCTURE_SPAWN) &&
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                          }
+                        }
+                    );
+
+    if (targets.length > 0) {
+      creepData.targetId = targets[0].id;
+    }
+  }
+
+}
+
 
 var roleHarvester = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    if(creep.store.getFreeCapacity() > 0) {
-      var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-      }
-    }
-    else {
-      var targets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                  structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-      }
-    });
-    if(targets.length > 0) {
-      if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-        // console.log( creep.name + " moving to " + targets[0].structureType + " at " + targets[0].pos);
-      }
-    }
-    else {
-        // help with the upgrading when they have nothing else to do
-        roleUpgrader.run(creep);
-      }
-    }
+
+    setCreepTargets(creep);
+    directCreepToWork(creep);
   }
+
 };
 
 module.exports = roleHarvester;
--- training/spawn.engine.js
+++ training/spawn.engine.js
@@ -1,7 +1,49 @@
 
+
+function cleanupAfterCreep(name) {
+  let creepData = Memory.creeps[name];
+  switch (creepData['role']) {
+    case 'builder':
+      if (creepData.heavy) {
+        Memory.spawnEngine.heavyBuilders--;
+      }
+      else {
+        Memory.spawnEngine.builders--;
+      }
+      break;
+    case 'harvester': 
+      Memory.spawnEngine.harvesters--;
+      break;
+    case 'upgrader': 
+      Memory.spawnEngine.upgraders--;
+      break;
+  }
+}
+
+function recordAddedCreep(name) {
+  let creepData = Memory.creeps[name];
+  switch (creepData['role']) {
+    case 'builder':
+      if (creepData.heavy) {
+        Memory.spawnEngine.heavyBuilders++;
+      }
+      else {
+        Memory.spawnEngine.builders++;
+      }
+      break;
+    case 'harvester': 
+      Memory.spawnEngine.harvesters++;
+      break;
+    case 'upgrader': 
+      Memory.spawnEngine.upgraders++;
+      break;
+  }
+}
+
 function cleanDeadCreeps() {
   for (var name in Memory.creeps) {
     if (!Game.creeps[name]) {
+      cleanupAfterCreep(name);
       delete Memory.creeps[name];
       console.log('Clearing non-existing creep memory:', name);
     }
@@ -21,33 +63,36 @@ function announceSpawning(spawn1) {
 
 function spawnCreep( spawn, body, opts ) {
   var newName = opts.memory.role + Game.time;
-  console.log('Spawning new harvester: ' + newName);
+  console.log('Spawning new Creep: ' + newName);
   let err = spawn.spawnCreep(body, newName, opts);
   if (err != OK) {
     console.log( "failed to spawn " + newName + " : " + err);
+  }
+  else {
+    recordAddedCreep(newName);
   }
   return err;
 }
 
 function spawnTheCreeps(spawn1) {
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  var heavyBuilders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.memory.heavy == true);
-  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  var harvesters = Memory.spawnEngine.harvesters;
+  var builders = Memory.spawnEngine.builders;
+  var heavyBuilders = Memory.spawnEngine.heavyBuilders;
+  var upgraders = Memory.spawnEngine.upgraders;
+  var constructionSites = Memory.spawnEngine.extensionSites;
 
-  var constructionSites = spawn1.room.find(FIND_MY_CONSTRUCTION_SITES);
-  
+  var minHarvesters = Memory.spawnEngine.minHarvesters;
+  var minBuilders = Memory.spawnEngine.minBuilders;
+  var minUpgraders = Memory.spawnEngine.minUpgraders;
+  var minHeavyHarvesters = Memory.spawnEngine.minHeavyHarvesters;
+  var maxHarvesters = Memory.spawnEngine.maxHarvesters;
+  var maxBuilders = Memory.spawnEngine.maxBuilders;
+  var maxUpgraders = Memory.spawnEngine.maxUpgraders;
+  var maxHeavyHarvesters = Memory.spawnEngine.maxHeavyHarvesters;
 
   var availableEnergy = spawn1.room.energyAvailable;
   var roomEnergyCapacity = spawn1.room.energyCapacityAvailable;
-  var minHarvesters = 1;
-  var minBuilders = 1;
-  var minUpgraders = 1;
-  var minHeavyHarvesters = 0;
-  var maxHarvesters = 5;
-  var maxBuilders = 5;
-  var maxUpgraders = 5;
-  var maxHeavyHarvesters = 5;
+
 
   if (constructionSites < 1) {
     minBuilders = 0;
@@ -56,30 +101,30 @@ function spawnTheCreeps(spawn1) {
   if (availableEnergy > 200) {
 
     if (!spawn1.spawning) {
-      if(harvesters.length < minHarvesters ) {
+      if(harvesters < minHarvesters ) {
           spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'harvester'}});
       }
-      else if(upgraders.length < minUpgraders) {
+      else if(upgraders < minUpgraders) {
         spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'upgrader'}});
       }
-      else if(builders.length < minBuilders) {
-        spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'builder', building: false}});
+      else if(builders < minBuilders) {
+        spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'builder', building: false, heavy: false}});
       }
-      else if( heavyBuilders.length < minHeavyHarvesters && availableEnergy > 400) {
+      else if( heavyBuilders < minHeavyHarvesters && availableEnergy > 400) {
         spawnCreep(spawn1, [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
                   {memory: {role: 'builder', building: false, heavy: true}});
       }
       else {
-        if(harvesters.length < maxHarvesters ) {
+        if(harvesters < maxHarvesters ) {
           spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'harvester'}});
         }
-        else if(upgraders.length < maxUpgraders) {
+        else if(upgraders < maxUpgraders) {
           spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'upgrader'}});
         }
-        else if(builders.length < maxBuilders) {
-          spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'builder', building: false}});
+        else if(builders < maxBuilders) {
+          spawnCreep(spawn1, [WORK,CARRY,MOVE], {memory: {role: 'builder', building: false, heavy: false}});
         }
-        else if( heavyBuilders.length < maxHeavyHarvesters && availableEnergy > 400) {
+        else if( heavyBuilders < maxHeavyHarvesters && availableEnergy > 400) {
           spawnCreep(spawn1, [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
                     {memory: {role: 'builder', building: false, heavy: true}});
         }
@@ -91,11 +136,37 @@ function spawnTheCreeps(spawn1) {
 
 var spawnEngine = {
 
-  run: function(creep) {
+  run: function() {
+
+    var spawn1 = Game.spawns['Spawn1'];
+
+    if ( Memory.spawnEngine == undefined) {
+      var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+      var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+      var heavyBuilders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.memory.heavy == true);
+      var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+      var constructionSites = spawn1.room.find(FIND_MY_CONSTRUCTION_SITES);
+
+      Memory.spawnEngine = { 
+        minHarvesters: 1,
+        minBuilders: 1,
+        minUpgraders: 1,
+        minHeavyHarvesters: 0,
+        maxHarvesters: 5,
+        maxBuilders: 5,
+        maxUpgraders: 5,
+        maxHeavyHarvesters: 5,
+
+        harvesters: harvesters.length,
+        builders: builders.length,
+        heavyBuilders: heavyBuilders.length,
+        upgraders: upgraders.length,
+        extensionSites: constructionSites.length
+      }
+    }
 
     cleanDeadCreeps();
 
-    var spawn1 = Game.spawns['Spawn1'];
     spawnTheCreeps(spawn1);
 
     announceSpawning(spawn1);
