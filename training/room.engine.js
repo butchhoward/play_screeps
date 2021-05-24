1f71eaa10534e8c7edea275ce994ef47101f8c0d
Butch Howard <butchhoward@gmail.com>
2021-05-24 15:08:41 -0400

    Check for  room exists before working
    Fails in the custom training room otherwise.

--- training/room.engine.js
+++ training/room.engine.js
@@ -1,6 +1,5 @@
 var sourcePicker = require("source.picker");
 
-
 function getCurrentRoom() {
   //assume I am in training mode and there is only one room
   //there has to be a better way to get a room from the collection!
@@ -12,8 +11,11 @@ function getCurrentRoom() {
   return room;
 }
 
-
 function buildExtensions(room) {
+  if (room === undefined || room.name === undefined) {
+    return;
+  }
+
   var maxExtensions = {
     1: 0,
     2: 5,
@@ -29,16 +31,16 @@ function buildExtensions(room) {
   });
   var constructionSites = room.find(FIND_MY_CONSTRUCTION_SITES, {
     filter: (site) => {
-      return ( site.structureType == STRUCTURE_EXTENSION &&
-               site.progress < site.progressTotal
-      );
+      return site.structureType == STRUCTURE_EXTENSION && site.progress < site.progressTotal;
     },
   });
-
 
   var pos = new RoomPosition(0, 0, room.name);
   if (extensions.length == 0) {
     var spawns = room.find(FIND_MY_SPAWNS);
+    if (spawns.length === 0) {
+      return;
+    }
     pos.x = spawns[0].pos.x;
     pos.y = spawns[0].pos.y;
   } else {
@@ -66,17 +68,16 @@ function buildExtensions(room) {
 }
 
 function layRoadBetween(room, startPos, endPos) {
-  console.log( "Layroad between " + startPos + " and " + endPos);
+  console.log("Layroad between " + startPos + " and " + endPos);
   var path = startPos.findPathTo(endPos);
-  for (let p in path ) {
+  for (let p in path) {
     const pos = path[p];
     room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
   }
 }
 
-
 function buildMainRoads(room) {
-  if (!('mainRoadsBuilt' in Memory.roomEngine)) {
+  if (!("mainRoadsBuilt" in Memory.roomEngine)) {
     Memory.roomEngine.mainRoadsBuilt = false;
   }
 
@@ -87,11 +88,10 @@ function buildMainRoads(room) {
   const keyFinds = [FIND_MY_SPAWNS, FIND_SOURCES];
   const keyStructures = [STRUCTURE_EXTENSION, STRUCTURE_SPAWN, STRUCTURE_CONTROLLER];
 
-
   var spawns = room.find(FIND_MY_SPAWNS);
-  for ( let s in spawns) {
+  for (let s in spawns) {
     let spawn = spawns[s];
-    let sourceId = sourcePicker.findSourceNear(room, spawn.pos );
+    let sourceId = sourcePicker.findSourceNear(room, spawn.pos);
     if (sourceId) {
       const source = Game.getObjectById(sourceId);
       layRoadBetween(room, spawn.pos, source.pos);
@@ -99,7 +99,7 @@ function buildMainRoads(room) {
     }
   }
 
-  var sourceId = sourcePicker.findSourceNear(room, room.controller.pos );
+  var sourceId = sourcePicker.findSourceNear(room, room.controller.pos);
   if (sourceId) {
     const source = Game.getObjectById(sourceId);
     layRoadBetween(room, room.controller.pos, source.pos);
@@ -110,12 +110,12 @@ function buildMainRoads(room) {
 
 var roomEngine = {
   run: function () {
-    if (!('roomEngine' in Memory)) {
+    if (!("roomEngine" in Memory)) {
       Memory.roomEngine = {};
     }
 
     var room = getCurrentRoom();
-    if (room == undefined) {
+    if (room === undefined) {
       console.log("room undefined");
       return false;
     }
@@ -128,4 +128,3 @@ var roomEngine = {
 };
 
 module.exports = roomEngine;
-
