557dbd5c9c4b9152d932f8be0260d97997c3c639
Butch Howard <butchhoward@gmail.com>
2021-05-27 19:35:21 -0400

    Be sure towers get juice when they need it

--- training/source.picker.js
+++ training/source.picker.js
@@ -120,8 +120,10 @@ function findPreferredStructureForTransferOfHarvest(room) {
   var targets = room.find(FIND_STRUCTURES, {
     filter: (structure) => {
       return (
-        (structure.structureType === STRUCTURE_EXTENSION ||
-          structure.structureType === STRUCTURE_SPAWN) &&
+        ( structure.structureType === STRUCTURE_TOWER ||
+          structure.structureType === STRUCTURE_EXTENSION ||
+          structure.structureType === STRUCTURE_SPAWN
+           ) &&
         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
       );
     },
